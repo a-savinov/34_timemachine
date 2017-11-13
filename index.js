c654fbdcba223f5942f7fa4e3010975395612ede
Alexey Savinov <belsat.net@gmail.com>
2017-11-13 17:37:46 +0300

    show random messages

--- index.js
+++ index.js
@@ -1,113 +1,131 @@
 var TIMEOUT_IN_SECS = 3 * 60
 var TEMPLATE = '<h1><span class="js-timer-minutes">00</span>:<span class="js-timer-seconds">00</span></h1>'
+var MOTTO = ['Больше дела – меньше слов.',
+             'Делу время, потехе час.',
+             'Душу вложишь - все сможешь.',
+             'За всякое дело берись умело.',
+             'Кончил дело — гуляй смело.'
+            ];
+var RANDOM_MGG_TIMEOUT = 30000
 
-function padZero(number){
-  return ("00" + String(number)).slice(-2);
+function padZero(number) {
+    return ("00" + String(number)).slice(-2);
 }
 
-class Timer{
-  // IE does not support new style classes yet
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
-  constructor(timeout_in_secs){
-    this.initial_timeout_in_secs = timeout_in_secs
-    this.reset()
-  }
-  getTimestampInSecs(){
-    var timestampInMilliseconds = new Date().getTime()
-    return Math.round(timestampInMilliseconds/1000)
-  }
-  start(){
-    if (this.isRunning)
-      return
-    this.timestampOnStart = this.getTimestampInSecs()
-    this.isRunning = true
-  }
-  stop(){
-    if (!this.isRunning)
-      return
-    this.timeout_in_secs = this.calculateSecsLeft()
-    this.timestampOnStart = null
-    this.isRunning = false
-  }
-  reset(timeout_in_secs){
-    this.isRunning = false
-    this.timestampOnStart = null
-    this.timeout_in_secs = this.initial_timeout_in_secs
-  }
-  calculateSecsLeft(){
-    if (!this.isRunning)
-      return this.timeout_in_secs
-    var currentTimestamp = this.getTimestampInSecs()
-    var secsGone = currentTimestamp - this.timestampOnStart
-    return Math.max(this.timeout_in_secs - secsGone, 0)
-  }
-}
-
-class TimerWidget{
-  // IE does not support new style classes yet
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
-  construct(){
-    this.timerContainer = this.minutes_element = this.seconds_element = null
-  }
-  mount(rootTag){
-    if (this.timerContainer)
-      this.unmount()
-
-    // adds HTML tag to current page
-    this.timerContainer = document.createElement('div')
-
-    this.timerContainer.setAttribute("style", "height: 100px;")
-    this.timerContainer.innerHTML = TEMPLATE
-
-    rootTag.insertBefore(this.timerContainer, rootTag.firstChild)
-
-    this.minutes_element = this.timerContainer.getElementsByClassName('js-timer-minutes')[0]
-    this.seconds_element = this.timerContainer.getElementsByClassName('js-timer-seconds')[0]
-  }
-  update(secsLeft){
-    var minutes = Math.floor(secsLeft / 60);
-    var seconds = secsLeft - minutes * 60;
-
-    this.minutes_element.innerHTML = padZero(minutes)
-    this.seconds_element.innerHTML = padZero(seconds)
-  }
-  unmount(){
-    if (!this.timerContainer)
-      return
-    this.timerContainer.remove()
-    this.timerContainer = this.minutes_element = this.seconds_element = null
-  }
-}
-
-
-function main(){
-
-  var timer = new Timer(TIMEOUT_IN_SECS)
-  var timerWiget = new TimerWidget()
-  var intervalId = null
-
-  timerWiget.mount(document.body)
-
-  function handleIntervalTick(){
-    var secsLeft = timer.calculateSecsLeft()
-    timerWiget.update(secsLeft)
-  }
-
-  function handleVisibilityChange(){
-    if (document.hidden) {
-      timer.stop()
-      clearInterval(intervalId)
-      intervalId = null
-    } else {
-      timer.start()
-      intervalId = intervalId || setInterval(handleIntervalTick, 300)
+class Timer {
+    // IE does not support new style classes yet
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+    constructor(timeout_in_secs) {
+        this.initial_timeout_in_secs = timeout_in_secs
+        this.reset()
     }
-  }
+    getTimestampInSecs() {
+        var timestampInMilliseconds = new Date().getTime()
+        return Math.round(timestampInMilliseconds / 1000)
+    }
+    start() {
+        if (this.isRunning)
+            return
+        this.timestampOnStart = this.getTimestampInSecs()
+        this.isRunning = true
+    }
+    stop() {
+        if (!this.isRunning)
+            return
+        this.timeout_in_secs = this.calculateSecsLeft()
+        this.timestampOnStart = null
+        this.isRunning = false
+    }
+    reset(timeout_in_secs) {
+        this.isRunning = false
+        this.timestampOnStart = null
+        this.timeout_in_secs = this.initial_timeout_in_secs
+    }
+    calculateSecsLeft() {
+        if (!this.isRunning)
+            return this.timeout_in_secs
+        var currentTimestamp = this.getTimestampInSecs()
+        var secsGone = currentTimestamp - this.timestampOnStart
+        return Math.max(this.timeout_in_secs - secsGone, 0)
+    }
+}
 
-  // https://developer.mozilla.org/en-US/docs/Web/API/Page_Visibility_API
-  document.addEventListener("visibilitychange", handleVisibilityChange, false);
-  handleVisibilityChange()
+class TimerWidget {
+    // IE does not support new style classes yet
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+    construct() {
+        this.timerContainer = this.minutes_element = this.seconds_element = null
+    }
+    mount(rootTag) {
+        if (this.timerContainer)
+            this.unmount()
+
+        // adds HTML tag to current page
+        this.timerContainer = document.createElement('div')
+
+        this.timerContainer.setAttribute("style", "height: 100px; position: fixed;z-index: 9999; top:25px; right:10px;")
+        this.timerContainer.innerHTML = TEMPLATE
+
+        rootTag.insertBefore(this.timerContainer, rootTag.firstChild)
+
+        this.minutes_element = this.timerContainer.getElementsByClassName('js-timer-minutes')[0]
+        this.seconds_element = this.timerContainer.getElementsByClassName('js-timer-seconds')[0]
+    }
+    update(secsLeft) {
+        var minutes = Math.floor(secsLeft / 60);
+        var seconds = secsLeft - minutes * 60;
+
+        this.minutes_element.innerHTML = padZero(minutes)
+        this.seconds_element.innerHTML = padZero(seconds)
+    }
+    unmount() {
+        if (!this.timerContainer)
+            return
+        this.timerContainer.remove()
+        this.timerContainer = this.minutes_element = this.seconds_element = null
+    }
+}
+
+
+function main() {
+
+    var timer = new Timer(TIMEOUT_IN_SECS)
+    var timerWiget = new TimerWidget()
+    var intervalId = null
+
+    timerWiget.mount(document.body)
+
+
+    function handleIntervalTick() {
+        var secsLeft = timer.calculateSecsLeft()
+        if (secsLeft === 0) {
+            timer.stop()
+            clearInterval(intervalId)
+            intervalId = null
+            var random_msg = function () {
+                alert(MOTTO[Math.floor(Math.random() * MOTTO.length)]);
+            };
+            setInterval(random_msg, RANDOM_MGG_TIMEOUT)
+        }
+        timerWiget.update(secsLeft)
+    }
+
+
+    function handleVisibilityChange() {
+        if (document.hidden) {
+            timer.stop()
+            clearInterval(intervalId)
+            intervalId = null
+        } else {
+            timer.start()
+            intervalId = intervalId || setInterval(handleIntervalTick, 300)
+        }
+    }
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/Page_Visibility_API
+    document.addEventListener("visibilitychange", handleVisibilityChange, false);
+    handleVisibilityChange()
 }
 
 // initialize timer when page ready for presentation
-window.addEventListener('load', main)
+window.addEventListener('load', main)window.addEventListener('load', main)
